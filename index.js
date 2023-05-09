6542afe4aeefeb1d9c9052d4526824f093173430
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-05-09 16:41:31 -0400

    inital updates

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+function newWeapon (url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('click', function(){
+        object.translate(100, 100)
+
+    
+})
+
+    object.addEventListener('dblclick', function(){
+        object.remove()
+
+
+})
+}
+
+newImage ('assets/green-character.gif', 100, 100)
+newImage ('assets/pine-tree.png', 450, 200)
+newImage ('assets/tree.png', 200, 300)
+newImage ('assets/tree.png', 800, 300)
+newImage ('assets/pillar.png', 350, 100)
+newImage ('assets/pillar.png', 550, 100)
+newImage ('assets/crate.png', 150, 200)
+newImage ('assets/well.png', 500, 425)
+newWeapon ('assets/sword.png', 500, 405)
+newWeapon ('assets/shield.png', 165, 185)
+newWeapon ('assets/staff.png', 600, 100)
+
+
+
